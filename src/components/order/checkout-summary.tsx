42276231b67cf7859662fc9a490ef6bf6a0cf055
Son Bui <sonbui2111s@gmail.com>
2026-01-20 01:43:30 +0700

    refactor

--- src/components/order/checkout-summary.tsx
+++ src/components/order/checkout-summary.tsx
@@ -6,7 +6,7 @@ export const CheckoutSummary = ({ cart }: { cart: Cart }) => {
   const currencyCode = cart.currencyCode ?? "USD";
 
   return (
-    <div className='text-[14px]!'>
+    <div>
       <h2>In your Cart</h2>
       <div>
         <div className='py-3 border-y border-neutral-200'>
@@ -24,7 +24,7 @@ export const CheckoutSummary = ({ cart }: { cart: Cart }) => {
           </div>
         </div>
 
-        <div className='flex items-center justify-between border-b border-neutral-200 text-(--theme-primary) py-3 font-bold text-[18px]!'>
+        <div className='flex items-center justify-between border-b border-neutral-200 text-(--theme-primary) py-3 font-bold text-[20px]!'>
           <p className='mb-0'>Total</p>
           <Price amount={cart.total} currencyCode={currencyCode} />
         </div>
@@ -47,7 +47,7 @@ export const CheckoutSummary = ({ cart }: { cart: Cart }) => {
 
                   <div className='flex flex-1 items-start justify-between gap-3'>
                     <div className='min-w-0'>
-                      <div className='truncate font-medium'>{item.title.slice(0, 20).concat("...")}</div>
+                      <div className='truncate font-medium'>{item.title.slice(0, 25).concat("...")}</div>
                       {item.variantOptions && (
                         <div className='mt-1 flex flex-wrap gap-1'>
                           {item.variantOptions.map((o) => (
